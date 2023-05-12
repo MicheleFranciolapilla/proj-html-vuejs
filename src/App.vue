f0d9953a26b9010cf925440a61339cdd65a562ba
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-05-12 20:51:30 +0900

    Struttura base

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <script>
 // Importazione dello store (state management)
   import { store } from "./store"
+  import Comp_Header from "./components/Comp_Header.vue";
+  import Comp_Main from "./components/Comp_Main.vue";
+  import Comp_Footer from "./components/Comp_Footer.vue";
+
   export default{
     name        : "App",
-    components  : {}
+    components  : 
+    {
+      Comp_Header,
+      Comp_Main,
+      Comp_Footer
+    }
   }
 </script>
 
 <template>
-  <h1>hello world</h1>
+  <Comp_Header/>
+  <Comp_Main/>
+  <Comp_Footer/>
 </template>
 
 <style scoped lang="scss">
---
+++ src/components/Comp_Footer.vue
@@ -0,0 +1,17 @@
+<script>
+    export default
+    {
+        name    : "Comp_Footer"
+    }
+</script>
+
+<template>
+    <div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    // Uso del foglio di stile scss
+    @use "../assets/style/main.scss" as *;
+
+</style>
