08bd2634287f937d8d170af0ae4e7c5a32149cdd
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-05-13 21:53:33 +0900

    Struttura cards con grid: spettacolare!!!

--- src/components/Comp_Main.vue
+++ src/components/Comp_Main.vue
@@ -1,6 +1,7 @@
 <script>
     import Comp_Menu from './Comp_Menu.vue';
     import Comp_Banner from './Comp_Banner.vue';
+    import Comp_Single_Card from './Comp_Single_Card.vue';
     export default
     {
         name        : "Comp_Main",
@@ -8,6 +9,7 @@
         {   
             Comp_Menu,
             Comp_Banner,
+            Comp_Single_Card
         },
         data()
         {
@@ -39,31 +41,52 @@
                                         text    : "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."     
                                     },
                                     {
-                                        image   : "img\blog-post3-600x900.jpg", 
+                                        image   : "img/blog-post3-600x900.jpg", 
                                         title   : "While my guitar gently weeps",
                                         text    : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."     
                                     },
                                     {
-                                        image   : "img\blog-post4-600x900.jpg", 
+                                        image   : "img/blog-post4-600x900.jpg", 
                                         title   : "It just sounds better",
                                         text    : "Aliquet risus feugiat in ante metus dictum at. Laoreet non curabitur gravida arcu ac. Lectus proin nibh nisl condimentum."     
                                     },
                                     {
-                                        image   : "img\blog_flavor_rock-1200x600.jpg",
+                                        image   : "img/blog_flavor_rock-1200x600.jpg",
                                         title   : "The flavor of rock",
                                         text    : "Nunc aliquet bibendum enim facilisis. Habitant morbi tristique senectus et netus et malesuada."     
                                     },
                                     {
-                                        image   : "img\blog-post1-600x900.jpg", 
+                                        image   : "img/blog-post1-600x900.jpg", 
                                         title   : "Taking it back to the old school",
                                         text    : "Tellus molestie nunc non blandit massa enim nec. Aliquam faucibus purus in massa tempor nec feugiat. Vulputate enim nulla aliquet porttitor lacus. "     
                                     },
                                     {
-                                        image   : "img\blog-post2-600x900.jpg", 
+                                        image   : "img/blog-post2-600x900.jpg", 
                                         title   : "Sharing the stage with a legend",
                                         text    : " Mauris sit amet massa vitae tortor condimentum lacinia. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis."     
                                     }
                                 ]
+            }
+        },
+        methods:
+        {
+            set_card_classes(index)
+            {
+                switch (index)
+                {
+                    case 0:
+                        return "first";
+                    case 1:
+                        return "second";
+                    case 2:
+                        return "third";
+                    case 3:
+                        return "fourth";
+                    case 4:
+                        return "fifth";
+                    case 5:
+                        return "sixth";
+                }
             }
         }
     }
@@ -82,7 +105,12 @@
         <section id="card_set_section">
             <Comp_Banner :banner_data = "banner_array[1]" />
             <div id="card_set">
-
+                <div class="card" 
+                v-for="(item, index) in cards_array"
+                :key="index"
+                :class="set_card_classes(index)">
+                    <!-- <Comp_Single_Card :current_card = "item" :index = "index" /> -->
+                </div>
             </div>
         </section>
     </main>
@@ -109,6 +137,78 @@
             #card_set
             {
                 padding: $card_set_padding;
+                width: $card_set_width;
+                // max-width: calc($card_set_width + $card_set_gap);
+                height: $card_set_height;
+                // max-height: calc($card_set_height + $card_set_gap);
+                // display: flex;
+                // flex-flow: column wrap;
+                // justify-content: start;
+                // align-items: start;
+                display: grid;
+                gap: $card_set_gap;
+                grid-template-areas: 
+                    "first first fifth"
+                    "second third fifth"
+                    "second third sixth"
+                    "fourth fourth sixth";
+                border: 3px solid yellow;
+                margin: 0 auto;
+                .card
+                {
+                    // width: $card_long_side;
+                    // height: $card_short_side;
+                    // flex-grow: 1;
+                    // border: 3px solid green;
+                    // &.vertical
+                    // {
+                    //     width: $card_short_side;
+                    //     height: $card_long_side;
+                    //     border: 3px solid white;
+                    // }
+                    // &.inner_flex
+                    // {
+                    //     width: $card_short_side;
+                    //     height: $card_long_side;
+                    //     border: 3px solid blueviolet;
+                    // }
+                }
+                .card:nth-child(1)
+                {
+                    grid-area: first;
+                    border: 3px solid white;
+                }
+                .card:nth-child(2)
+                {
+                    grid-area: second;
+                    border: 3px solid yellow;
+
+                }
+                .card:nth-child(3)
+                {
+                    grid-area: third;
+                    border: 3px solid yellow;
+
+                }
+                .card:nth-child(4)
+                {
+                    grid-area: fourth;
+                    border: 3px solid white;
+
+                }
+                .card:nth-child(5)
+                {
+                    grid-area: fifth;
+                    border: 3px solid blue;
+
+                }
+                .card:nth-child(6)
+                {
+                    grid-area: sixth;
+                    border: 3px solid blue;
+
+                }
+
             } 
         }
     }
---
+++ src/components/Comp_Single_Card.vue
@@ -0,0 +1,47 @@
+<script>
+    export default
+    {
+        name        : "Comp_Single_Card",
+        props       : ['current_card', 'index'],
+        methods:
+        {
+            set_card_classes()
+            {
+                if ((this.index == 0) || (this.index == 3))
+                    return "horizontal";
+                else if ((this.index == 4) || (this.index == 5))
+                    return "vertical_with_grow";
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="card" :class="set_card_classes()">
+        <!-- <img :src="current_card.image" alt=""> -->
+    </div>
+</template>
+
+<style scoped lang="scss">
+    // Uso del foglio di stile scss
+    @use "../assets/style/main.scss" as *;
+
+        // .card
+        // {
+        //     width: $card_short_side;
+        //     height: $card_long_side;
+        //     border: 3px solid green;
+        //     &.horizontal
+        //     {
+        //         width: $card_long_side;
+        //         height: $card_short_side;
+        //         border: 3px solid white;
+
+        //     }
+        //     &.vertical_with_grow
+        //     {
+        //         flex-grow: 1;
+        //         border: 3px solid red;
+        //     }
+        // }
+</style>
